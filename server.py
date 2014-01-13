9f53175e2171d6a9be9073e77193200933943564
Yuwei <mark@uws53599usr.uws.ualberta.ca>
2014-01-13 16:11:41 -0700

    goog

--- server.py
+++ server.py
@@ -3,13 +3,14 @@ import SocketServer
 import SimpleHTTPServer
 import os
 import time
+import posixpath
 # Copyright 2013 Abram Hindle, Eddie Antonio Santos
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 # 
-#     http://www.apache.org/licenses/LICENSE-2.0
+#	 http://www.apache.org/licenses/LICENSE-2.0
 # 
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -28,15 +29,15 @@ import time
 
 # try: curl -v -X GET http://127.0.0.1:8080/
 DEFAULT_ERROR_MESSAGE = """\
-    <head>
-    <title>Error response</title>
-    </head>
-    <body>
-    <h1>Error response</h1>
-    <p>Error code 404.
-    <p>Message: NOT FOUND.
-    <p>Error code explanation: 404 = FILE NOT FOUND.
-    </body>
+	<head>
+	<title>Error response</title>
+	</head>
+	<body>
+	<h1>Error response</h1>
+	<p>Error code 404.
+	<p>Message: NOT FOUND.
+	<p>Error code explanation: 404 = FILE NOT FOUND.
+	</body>
 """
 class MyWebServer(SocketServer.BaseRequestHandler):
 	code = 200
@@ -44,16 +45,21 @@ class MyWebServer(SocketServer.BaseRequestHandler):
 	def handle(self):
 		self.data = self.request.recv(1024).strip()
 		request_list = self.data.split()
-		path =request_list[1][1:]
+		path=posixpath.normpath(request_list[1])
+		path=path[1:];
 		if path == '':
 			path = 'index.html'
-		if os.access(path, os.R_OK) is False:
+		if os.path.isdir(path):
+			path = path + '/index.html'
+		path=posixpath.normpath(path)
+		print path
+		if  os.access(path, os.R_OK) is False:
 			self.request.sendall("HTTP/1.1 404 NOT FOUND\r\n")
 			self.send_header('Date', self.date_time_string())
 			self.send_header('Content-Type',"text/plain; charset=UTF-8")
 			self.end_header()
 			self.request.sendall(DEFAULT_ERROR_MESSAGE)
-		if os.access(path, os.R_OK)==True:
+		if os.access(path, os.R_OK) is True:
 			name,extend = os.path.splitext(path)
 			try:
 				with open(path,'r') as f:
@@ -74,21 +80,21 @@ class MyWebServer(SocketServer.BaseRequestHandler):
 		timestamp = time.time()
 		year, month, day, hh, mm, ss, wd, y, z = time.gmtime(timestamp)
 		s = "%s, %02d %3s %4d %02d:%02d:%02d GMT" % (
-                self.weekdayname[wd],
-                day, self.monthname[month], year,
-                hh, mm, ss)
+				self.weekdayname[wd],
+				day, self.monthname[month], year,
+				hh, mm, ss)
 		return s
 	weekdayname = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
 	monthname = [None,
-                 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
-                 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+				 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+				 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 if __name__ == "__main__":
 	HOST, PORT = "localhost", 8080
 	os.chdir('www')
 	SocketServer.TCPServer.allow_reuse_address = True
-    # Create the server, binding to localhost on port 9999
+	# Create the server, binding to localhost on port 9999
 	server = SocketServer.TCPServer((HOST, PORT), MyWebServer)
-    # Activate the server; this will keep running until you
-    # interrupt the program with Ctrl-C
+	# Activate the server; this will keep running until you
+	# interrupt the program with Ctrl-C
 	server.serve_forever()
 
---
+++ test.py
@@ -0,0 +1,33 @@
+import os
+import posixpath
+import urllib
+def translate_path(path):
+        """Translate a /-separated PATH to the local filename syntax.
+
+        Components that mean special things to the local file system
+        (e.g. drive or directory names) are ignored.  (XXX They should
+        probably be diagnosed.)
+
+        """
+        # abandon query parameters
+        path = path.split('?',1)[0]
+        path = path.split('#',1)[0]
+        # Don't forget explicit trailing slash when normalizing. Issue17324
+        trailing_slash = path.rstrip().endswith('/')
+        path = posixpath.normpath(path)
+        words = path.split('/')
+        words = filter(None, words)
+        path = os.getcwd()
+        print words
+        for word in words:
+            drive, word = os.path.splitdrive(word)
+            head, word = os.path.split(word)
+            if word in (os.curdir, os.pardir): continue
+            path = os.path.join(path, word)
+        if trailing_slash:
+            path += '/'
+        return path
+a = translate_path("/../../../../index.html")
+print a
+c = "/dasda/dasdasd/"
+print c.rstrip().endswith('/')
